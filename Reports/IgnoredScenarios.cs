42fabfa26a4ef6de2f756b415570d25ea014d8b3
Graham Bull <graham.bull@newday.co.uk>
2024-08-15 10:14:02 +0100

    change to static class; don't use Interlocked.Add to avoid potential confusion

--- Reports/IgnoredScenarios.cs
+++ Reports/IgnoredScenarios.cs
@@ -4,11 +4,11 @@
 /// Use this to keep track of scenarios that are ignored using StepExecution.Current.IgnoreScenario().
 /// LightBDD doesn't include ignored scenarios when it calls IReportFormatter.Format().
 /// </summary>
-public abstract class IgnoredScenarios
+public static class IgnoredScenarios
 {
     private static int _count = 0;
 
-    public static int Count => Interlocked.Add(ref _count, 0);
+    public static int Count => _count;
 
     public static void Increment() => Interlocked.Increment(ref _count);
 }
